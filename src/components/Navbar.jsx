fe71a756a156ed143d16676de824fcb36885b84c
KarlozOso <135464400+KarlozOso@users.noreply.github.com>
2023-08-13 10:07:50 +0000

    caminata

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose} from "react-icons/ai"
+import {BsFillCartFill} from "resct-icons/bs"
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+      <div className='flex items-center'>
+        <div className='cursor-pointer'v>
+      <AiOutlineMenu size={30} \>
+      <h1 className=' text-2xl sm:text-3xl lg:text-4xl px-2'> Best <span className='font-bold'> Foods</span>
+      </h1>
+        </div>
+        <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]' > 
+          <p className='bg-black text-white rounded-full p-2'> Delivery</p>
+          <p className='p-2'>Pick Up</p>
+        </div>
+        </div>
+        <div className='bg-gray-200 rounded-full flex items-center px-2 width-[200px] sm:w-[400px] lg:w-[500px]'>
+        <AiOutlineSearch size={25}/>
+          <input className='transparent p-2 focus:outline-none' type='text' placeholder='Search Food'/>
+        </div>
+      <button className='bg-black text-white hidden md:flex items-cente py-2 rounded-full'> 
+        <BsFillCartFill size={20} className='mr-2'/>
+        Cart </button>
+
+<div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+      <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+      <AiOutlineClose size={30} className='absolute rigth-4 top-4 cursor-pointer'/> 
+        <h2 className='text-2x p-4 '>Best <span className='font-bold'>Foods</span></h2>
+      </div>
+    </div>
   )
 }
 
